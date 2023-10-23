8187641e1703a8df40c2417349d8a598baa42225
Tobias Santos <tobitosan@gmail.com>
2023-10-23 17:24:07 -0300

    Adição de JS e socialicons
    script.js adicionado - adição e configuração do switch e botão no index.html e main.css - adição e configuração dos social icons

---
+++ script.js
@@ -0,0 +1,19 @@
+function togleMode() {
+  const html = document.documentElement;
+  html.classList.toggle('light')
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', 'assets/tobi.jpg')
+    img.setAttribute('alt', 'Foto de Tobias Santos com camisa cinza e mochila preta, segurando uma caveira preta de óculos em frente ao seu rosto, prateleiras de loja com caixas de som ao fundo.')
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+   img.setAttribute('src', 'assets/avatar.png')
+   img.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo.')
+  }
+
+}
